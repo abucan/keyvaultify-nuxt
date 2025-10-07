9a1783a8a05b4b69446ac22501c55b742bb2c4e2
Ante Bucan <ab8390@g.rit.edu>
2025-10-07 08:13:31 +0200

    Refactor Tailwind CSS setup and update utility functions. Changed Tailwind CSS import path and updated CSS variables in main.css. Replaced utility function location from lib/utils to utils/cn, and removed the old utils.ts file. Updated app.vue for improved layout and button styling.

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <div>
+    <div class='w-full h-screen flex flex-col justify-center items-center gap-4 bg-blue-100'>
       <h1 class="text-3xl font-bold">Hello World</h1>
-      <ui-button variant='destructive'>Click me</ui-button>
+      <ui-button variant='destructive' class='text-white'>Click me</ui-button>
     </div>
-  </div>
 </template>
